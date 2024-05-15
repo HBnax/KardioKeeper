3d0bcc1963c37bfea756e83dab67da497f54a96e
Halle Broadnax <broadnax@chapman.edu>
2024-05-14 22:03:04 -0700

    Update auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers:[
     {
-      domain:'https://equal-moth-69.clerk.accounts.dev',
-      applicationID:"convex", 
+      domain:'https://engaging-pheasant-78.clerk.accounts.dev',
+      applicationID: "convex", 
     }
   ]
 }
